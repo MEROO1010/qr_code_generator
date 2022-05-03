2c3c9f4bbe0b24e22044afc14f6c80d2874f2d94
marwah alsaggaf <meroo1995saggaf@outlook.com>
2022-05-03 09:47:51 +0300

    first commit

---
+++ lib/ui_style/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppStyle{
+  static Color primaryColor = Color(0xFFFFF59D);
+  static Color textInputColor = Color(0xFF9E9E9E);
+  static Color accentColor = Color(0xFF4c8842);
+}
